cab651751c45b5cd9c81dc3ed07f904937bf1249
shullmb <shullmb@gmail.com>
2018-07-03 19:08:24 -0700

    refactored film-list and film-details to components

---
+++ src/FilmList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FilmList extends Component {
+  
+  render() {
+    return (
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+      </div>
+    );
+  }  
+}
+
+export default FilmList;
