9b476b7d1ada03dbfb84809633d21b2710ce8f4a
shullmb <shullmb@gmail.com>
2018-07-03 19:21:10 -0700

    passing props to FilmListing

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class FilmListing extends Component {
-  
   render() {
+    let allFilms = this.props.films.map( (film) => {
+      return (
+        <h1>{film.title}</h1>
+      )
+    })
+
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
+          {allFilms};
       </div>
     );
   }  
