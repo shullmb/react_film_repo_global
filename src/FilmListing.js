c8e0acfd1eef3d3b4d708006b1d26d262762f9cb
shullmb <shullmb@gmail.com>
2018-07-06 08:45:39 -0700

    added conditional for filter is-active

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -5,31 +5,48 @@ class FilmListing extends Component {
   constructor(props) {
     super(props)
     this.handleFilterClick = this.handleFilterClick.bind(this)
+    this.state = {
+      filter: 'all'
+    }
   }
 
   handleFilterClick(filter) {
+    this.setState({
+      filter: filter === 'all' ? 'all' : 'faves'
+    })
     console.log('Setting filter to ', filter, '...')
   }
   
   render() {
+    
     const allFilms = this.props.films.map( film => {
       return (
         <FilmRow film={film} key={film.id}/>
       )
     })
 
+    let allIsActive;
+    let faveIsActive;
+    if (this.state.filter === 'all') {
+      allIsActive = 'is-active'
+      faveIsActive = ''
+    } else {
+      faveIsActive = 'is-active'
+      allIsActive = ''
+    }
+    
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
         <div className="film-list-filters">
-          <div className="film-list-filter"
+          <div className={`film-list-filter ${allIsActive}`}
                onClick={ () => {this.handleFilterClick('all')}}>
-            ALL
+                ALL
             <span className="section-count">{this.props.films.length}</span>
           </div>
-          <div className="film-list-filter"
+          <div className={`film-list-filter ${faveIsActive}`}
                onClick={ () => {this.handleFilterClick('faves')}}>
-            FAVES
+                FAVES
             <span className="section-count">0</span>
           </div>
         </div>
